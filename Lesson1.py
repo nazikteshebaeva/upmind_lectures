8daf53fee5ee755c738013fd7f7039d7b4c24de8
unknown <teshebaevanazik99@gmail.com>
2025-03-21 02:41:23 -0400

    first commit

---
+++ Lesson1.py
@@ -0,0 +1,81 @@
+# Theme : DataTypes; INTSTRING; FLOAT; BOOL
+# one sentence comment
+
+"""
+multiple sentences comment
+1
+2
+3
+etc
+"""
+
+# int (integer) - only numbers like (1,2,45 etc), without dots like (1.25, 65.89)
+# num1 = 1
+# num2 = 2
+# num3 = 3
+
+#str - (string) unmodifiable string data type
+# # s1 = "string"
+# s2 = "string"
+
+# print(num1)
+# print() - console output, takes as input variable van be comma separated
+# input() - console input, takes as input the text that will be output to the console for input
+# type() - outputs the variable data type, takes a variable as input
+# len() - outputs the lenght of the object
+# str() - string
+# int() - integer
+#float() - float
+# lower() - string method converting all lowercase characters to lowercase
+
+# user_number = input("Enter a number:")
+
+# print("User entered: ", type(int(user_number)))
+
+# age = int(input("write your age: ")) # input the number
+# print(type(age)) # <class 'int'>
+
+# a = 10
+# b = 3
+#
+# print(a + b) # 13 (addition)
+# print(a - b) # 7 (subtraction)
+# print(a * b) # 30 (multiplication)
+# print(a // b) # 3 (integer division)
+# print(a % b) # 1 (remainder of division)
+# print(a ** b) # 1000 (enlarging)
+
+# pi = float(input("enter a floating point number: ")) # Input float
+# print("You have entered:", pi)
+# print(type(pi)) # <class 'float'>
+# print(round(pi, 2)) # Rounding
+#
+# num_str = "3.5"
+# num_float = float(num_str)
+# print(type(num_float)) # <class 'float'>
+#
+# # str - (string) unmodifiable string data type
+# s1 = ("string")
+# enter = 'Login'
+
+# a = 3
+# b = 10
+# is_python_easy = input("Is Python easy? (yes/no): ") == "yes" # Input bool
+# print("Your answer:", is_python_easy)
+# print(type(is_python_easy)) # <class 'bool'>
+# print(a > b) # False
+# print(a < b) # True
+# print(a == b) #False
+# print(a != b) #True
+# print(True and False) # False
+# print(True or False) # True
+# print(not True) # False
+
+# # Casting
+# print(float(5)) #5.0
+# print(int(5.9)) #5
+# print(int("100")) #100
+# print(str(42)) # "42"
+# print(int(True)) # 1
+# print(int(False)) # 0
+
