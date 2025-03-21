547875060314b7bdc85fa2483bac4dc9db5c1cc2
unknown <teshebaevanazik99@gmail.com>
2025-03-21 03:00:30 -0400

    fourth commit

---
+++ Homework5.py
@@ -0,0 +1,212 @@
+#1
+# film = ["Titanic", "The Big Bang Theory", "Twilight", "Iron Man", "Emilie in Paris"]
+# print(film[1])
+
+#2
+# numbers = [10, 20,30, 40, 50]
+# numbers[2] = 99
+# print(numbers)
+
+#3
+# string1 = ["apple", "banana", "orange"]
+# string2 = ["cat", "dog", "tiger"]
+# concatenated_list = string1 + string2
+# print(concatenated_list)
+
+#4
+# list1 = [4, 2, 9, 1]
+# list1.append(7)
+# list1.remove(2)
+# print(list1)
+
+#5
+# list2 = ["Python", "Java", "C++", "JavaScript", "PHP"]
+# print(list2[0:3])
+
+#6
+# names = ["Anna", "Boris", "Vika"]
+# print(names[1])
+
+#7
+# numbers = [5, 10, 15, 20, 25]
+# print(numbers[::-1])
+
+#8
+# string1 = []
+# string1.append(5)
+# string1.append(10)
+# string1.append(15)
+# print(string1)
+
+#9
+# numbers = [10, 15, 20, 25, 30]
+# print(numbers[0])
+# print(numbers[-1])
+
+#10
+# string1 = [1, 2, 3]
+# string2 = [4, 5, 6]
+# concatenated_list = string1 + string2
+# print(concatenated_list)
+
+#11
+#firsts version
+# numbers = [2, 7, 3, 8, 5, 9]
+# print(sum(numbers[0:3]))
+
+#second version
+# number = [2, 7, 3, 8, 5, 9]
+# num1 = 2
+# num2 = 7
+# num3 = 3
+# print(num1 + num2 + num3)
+
+#12
+# string = ["apple", "banana", "cherry", "date"]
+# print(len(string[0]))
+
+#13
+# list1 = [100, 200, 300, 400, 500]
+# list1.reverse()
+# print(list1)
+
+#14
+# list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(list1[0:5])
+
+#15
+# string1 = ["cat", "dog", "tiger", "lion", "elephant"]
+# print(string1[4])
+
+#16
+# num1 = [34, 45, 56]
+# print(max(num1))
+# print(min(num1))
+
+#17 work on this after
+# words1 = ["apple", "banana", "grape", "melon"]
+# if "pear" in words1:
+#     print("It is in")
+# else:
+#     print("It is not in")
+
+#18
+# string1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# string1[3] = "four"
+# print(string1)
+
+#19
+# numbers = [3, 6, 9, 12, 15]
+# num1 = 9
+# print(num1 * 2)
+# num2 = (num1 * 2)
+# numbers[2] = num2
+# print(numbers)
+
+#20
+# string1 = [[1, 2], [3, 4], [5, 6]]
+# print(string1[1][1])
+
+#21
+# string1 = [10, 20, 30, 40, 50]
+# string1.insert(2, 25)
+# print(string1)
+
+#22
+# string1 = ["one", "two", "three", "four"]
+# string1[0] = "ONE"
+# print(string1)
+
+#23
+# numbers = [4, 16, 25, 36]
+# num1 = 4
+# print(pow(4, 2))
+
+#24
+# string0 = [5, 10, 15, 20]
+# print(max(string0))
+# string1 = max(string0)
+# print(min(string0))
+# string2 = min(string0)
+# print(string1 - string2)
+
+#25 work on it
+# names = ["Begaiym", "Rufina", "Aigul", "Kanykei", "Nurzat"]
+
+
+#26
+# string3 = [1, 1, 2, 2, 3, 3, 4, 4]
+# print(string3.count(2))
+
+#27 !!!
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(numbers[0:3])
+# print(numbers[6:9])
+
+#28
+# string1 = ["python", "java", "c++", "javascript"]
+# string1[1] = "JAVA"
+# print(string1)
+
+#29
+# string1 = [1, 2, 3]
+# string2 = [4, 5, 6]
+# concatenated_list = string1 + string2
+# print(concatenated_list)
+
+#30
+# numbers = [10, 20, 30, 40, 50]
+# print(numbers[0])
+# print(numbers[4])
+
+#31
+# string1 = ["Alex", "Masha", "Oleg", "Vika"]
+# if "Oleg" in string1:
+#     print("Yes")
+# else:
+#     print("No")
+
+#32
+# numbers = [3, 6, 9, 12]
+# numbers[2] = 99
+# print(numbers)
+
+#33
+# string = ["cat", "dog", "tiger"]
+# string[0] = "cat, tiger"
+# print(string)
+# string.remove("tiger")
+# print(string)
+
+#34 work on this
+# numbers = [5, -2, 3, -1, 6, -3]
+
+
+#35!!!
+# str1 = [10, 20, 30, 40, 50]
+# print(str1[0:2])
+
+#36
+# numbers = [1, 2, 3, 4, 5]
+# num1 = 1
+# print(pow(1, 2))
+
+#37
+# str2 = ["hello", "world", "python"]
+# str2.remove("world")
+# print(str2)
+
+#38
+# numbers = [12, 5, 8, 15, 30]
+# num1 = 5
+# num2 = 8
+# print(num1 - num2)
+
+#39!!!
+# words = ["apple", "banana", "cherry", "date"]
+# word1 = "banana"
+# word1.reverse("banana")
+
+#40
+# str3 = [[2, 4, 6], [1, 3, 5], [8, 10, 12]]
+# print(str3[1][0])
